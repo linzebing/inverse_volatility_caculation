fd0d6ad99c7892e513119b08004fcf3d45fd1e01
linzebing <zebingl@fb.com>
2020-02-15 22:16:35 -0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # inverse_volatility_caculation
 This is to help people get forward signal of their inverse volatility allocation strategy. https://www.portfoliovisualizer.com/ used to provide this for free, but now it requires a subscription.
+
+If you are interested why this may help build your portfolio, see https://quantdare.com/risk-parity-versus-inverse-volatility/
+
+## Preparation
+```
+pip3 install numpy
+pip3 install requests
+```
+
+## Example Usage
+```
+./inverse_volatility.py
+Profolio: ['UPRO', 'TMF'], as of 2020-02-15 (window size is 20 days)
+UPRO allocation ratio: 49.09%, anualized volatility: 39.87%, performance: 5.36%
+TMF allocation ratio: 50.91%, anualized volatility: 38.45%, performance: 11.64%
+```
+
+Checking against Portfolio Visualizer: ![](UPRO_TMF.png)
+
+```
+Profolio: ['UPRO', 'VOO', 'EDV'], as of 2020-02-15 (window size is 20 days)
+UPRO allocation ratio: 15.79% (anualized volatility: 39.87%, performance: 5.36%)
+VOO allocation ratio: 47.22% (anualized volatility: 13.33%, performance: 2.06%)
+EDV allocation ratio: 36.98% (anualized volatility: 17.03%, performance: 5.63%)
+```
+
+Checking against Portfolio Visualizer: ![](UPRO_VOO_EDV.png)
+
+## Note
+
+Data source: https://finance.yahoo.com/quote/VOO/history?p=VOO -> Historical Data -> Download Data
+
+Note that the cookie in the code may expire that you need to update the `crumb` parameter in the url along with your cookie from browser accordingly.
---
+++ inverse_volatility.py
@@ -0,0 +1,55 @@
+#!/usr/local/bin/python3
+
+from datetime import datetime, date
+import math
+import numpy as np
+import time
+import sys
+import requests
+
+if len(sys.argv) == 1:
+    symbols = ['UPRO', 'TMF']
+else:
+    symbols = sys.argv[1].split(',')
+    for i in range(len(symbols)):
+        symbols[i] = symbols[i].strip().upper()
+
+num_trading_days_per_year = 252
+window_size = 20
+date_format = "%Y-%m-%d"
+end_timestamp = int(time.time()) + 1000000
+start_timestamp = int(end_timestamp - 1000000 - (1.4 * (window_size + 1) + 10) * 86400)
+
+
+def get_volatility_and_performance(symbol):
+    download_url = "https://query1.finance.yahoo.com/v7/finance/download/{}?period1={}&period2={}&interval=1d&events=history&crumb=a7pcO//zvcW".format(symbol, start_timestamp, end_timestamp)
+    lines = requests.get(download_url, cookies={'B': 'chjes25epq9b6&b=3&s=18'}).text.strip().split('\n')
+    assert lines[0].split(',')[0] == 'Date'
+    assert lines[0].split(',')[4] == 'Close'
+    prices = []
+    for line in lines[1:]:
+        prices.append(float(line.split(',')[4]))
+    prices.reverse()
+    volatilities_in_window = []
+
+    for i in range(window_size):
+        volatilities_in_window.append(math.log(prices[i] / prices[i+1]))
+        
+    most_recent_date = datetime.strptime(lines[-1].split(',')[0], date_format).date()
+    assert (date.today() - most_recent_date).days <= 2, "today is {}, most recent trading day is {}".format(today, most_recent_date)
+
+    return np.std(volatilities_in_window, ddof = 1) * np.sqrt(num_trading_days_per_year), prices[0] / prices[20] - 1.0
+
+volatilities = []
+performances = []
+sum_inverse_volatility = 0.0
+for symbol in symbols:
+    volatility, performance = get_volatility_and_performance(symbol)
+    sum_inverse_volatility += 1 / volatility
+    volatilities.append(volatility)
+    performances.append(performance)
+
+print ("Profolio: {}, as of {} (window size is {} days)".format(str(symbols), date.today().strftime('%Y-%m-%d'), window_size))
+for i in range(len(symbols)):
+    print ('{} allocation ratio: {:.2f}% (anualized volatility: {:.2f}%, performance: {:.2f}%)'.format(symbols[i], float(100 / (volatilities[i] * sum_inverse_volatility)), float(volatilities[i] * 100), float(performances[i] * 100)))
+
